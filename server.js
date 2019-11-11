86255fe49d849914d65a386ccf0ce5e1400246ac
DarioSusic <dario.susic@gmail.com>
2019-11-12 00:26:37 +0100

    added auth midleware, setup for react and connection to database

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const connectDB = require('./config/db');
+const path = require('path');
 
+// Connect Database
+connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-app.listen(port, () => {
-    console.log(`App running at port: ${port}`);
-})
+// Define Routes
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
