bd274bfffc2857a5aae3d338339f343ce7697a22
DarioSusic <dario.susic@gmail.com>
2019-12-03 14:48:12 +0100

    minor changers

--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+const PORT = process.env.PORT || 4000;
+
 // Define Routes
 app.use('/', express.static('../client/build'));
 app.use('/api/users', require('./routes/api/users'));
-
-const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
