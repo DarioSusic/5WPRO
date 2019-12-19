0a657ba0ef64f80fb907d4d7668162f17a2c2830
DarioSusic <dario.susic@gmail.com>
2019-12-19 16:17:17 +0100

    refactoring product creation

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -10,7 +10,7 @@ const User = require('../../models/User');
 //@route    GET api/product
 //@desc     Get all products
 //@access   public
-router.get('/', auth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const products = await Product.find().limit(20);
     res.json(products);
@@ -40,7 +40,7 @@ router.get('/:id', async (req, res) => {
 
 //@route    POST api/product
 //@desc     Create new product
-//@access   private
+//@access   private / Restricted
 router.post(
   '/',
   [
@@ -67,10 +67,10 @@ router.post(
       const user = await User.findById(req.user.id).select('-password');
 
       if (!user.isAdmin || !user.isSuperAdmin) {
-        res.status(401).json({ msg: 'Unouthorised access' });
+        res.status(401).json({ msg: 'Unauthorised access' });
       }
 
-      //TODO add other fields from product schema
+      //FIXME add other fields from product schema
       const newProduct = new Product({
         title: req.body.title,
         description: req.body.description,
@@ -94,17 +94,18 @@ router.post(
 
 //@route    DELETE api/product/:id
 //@desc     Delete product by id
-//@access   private
+//@access   private / Restricted
 router.delete('/:id', auth, async (req, res) => {
   try {
     const product = await Post.findById(req.params.id);
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user.isAdmin || !user.isSuperAdmin) {
+      res.status(401).json({ msg: 'Unauthorised access' });
+    }
 
     if (!product) {
       return res.status(404).json({ msg: 'Product does not exist' });
-    }
-
-    if (!user.isAdmin || !user.isSuperAdmin) {
-      res.status(401).json({ msg: 'Unouthorised access' });
     }
 
     await product.remove();
@@ -115,5 +116,114 @@ router.delete('/:id', auth, async (req, res) => {
     return res.status(500).json({ msg: 'Server Error' });
   }
 });
+
+//@route    PUT api/product/:id
+//@desc     Update product by id
+//@access   private / Restricted
+router.put('/:id', auth, async (req, res) => {
+  try {
+    const product = await Post.findById(req.params.id);
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user.isAdmin || !user.isSuperAdmin) {
+      res.status(401).json({ msg: 'Unauthorised access' });
+    }
+
+    if (!product) {
+      return res.status(404).json({ msg: 'Product does not exist' });
+    }
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+//@route    POST api/product
+//@desc     Create new product
+//@access   private / Restricted
+router.post(
+  '/1',
+  [
+    auth,
+    [
+      check('title', 'Title of product is required')
+        .not()
+        .isEmpty(),
+      check('quantity', 'Quantity of product is required')
+        .not()
+        .isEmpty(),
+      check('price', 'Price of product is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      description,
+      quantity,
+      price,
+      discount,
+      sku,
+      bar_code,
+      release_date,
+      weight,
+      width,
+      height,
+      depth
+    } = req.body;
+
+    //TODO finish product fields
+    const productFields = {};
+    productFields.user = req.user.id;
+    if (title) productFields.title = title;
+    if (description) productFields.title = description;
+    if (quantity) productFields.title = quantity;
+    if (sku) productFields.title = sku;
+
+    productFields.pricing = {};
+    if (price) productFields.pricing.price;
+    if (discount) productFields.pricing.discount;
+
+    productFields.manufacture_details = {};
+    if (bar_code) productFields.manufacture_details.bar_code;
+    if (release_date) productFields.manufacture_details.release_date;
+
+    productFields.shipping_details = {};
+    if (bar_code) productFields.manufacture_details.bar_code;
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      if (!user.isAdmin || !user.isSuperAdmin) {
+        res.status(401).json({ msg: 'Unauthorised access' });
+      }
+
+      //FIXME add other fields from product schema
+      const newProduct = new Product({
+        title: req.body.title,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        pricing: { price: req.body.price },
+        sku: req.body.sku,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const product = await newProduct.save();
+
+      res.json(product);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: 'Server Error' });
+    }
+  }
+);
 
 module.exports = router;
