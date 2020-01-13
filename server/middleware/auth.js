fdb4734c7034323a0c82658ee36576567797d0d2
DarioSusic <dario.susic@gmail.com>
2020-01-13 11:46:03 +0100

    fixed jwtsecret for heroku

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
 
 module.exports = function(req, res, next) {
   //get token from header
@@ -10,9 +9,17 @@ module.exports = function(req, res, next) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
+  let secret;
+  if (!process.env.HEROKU) {
+    const config = require('config');
+    secret = config.get('jwtSecret');
+  } else {
+    secret = process.env.jwtSecret;
+  }
+
   //verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, secret);
     req.user = decoded.user;
     next();
   } catch (err) {
